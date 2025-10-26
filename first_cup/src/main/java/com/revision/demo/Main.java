ec641b3f658845311ebaf5649040e4f52758d6e8
YeHtet1001 <java17springboot@gmail.com>
2025-10-26 20:12:56 +0630

    added

--- first_cup/src/main/java/com/revision/demo/Main.java
+++ first_cup/src/main/java/com/revision/demo/Main.java
@@ -3,5 +3,16 @@ package com.revision.demo;
 public class Main {
     public static void main(String[] args) {
 
+        Employee emp = new Employee();
+
+        emp.setId(1);
+        emp.setName("John Doe");
+        emp.setAge(22);
+        emp.setPosition("Developer");
+
+        System.out.println(emp.toString());
+
+        System.out.println(emp.showDetails());
+
     }
 }
